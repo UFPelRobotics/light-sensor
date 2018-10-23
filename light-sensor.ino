5cf13f6bd86589e482d3b8a83b900ab38305b5a7
William Kolodziejski <william.kol.wk@gmail.com>
2018-10-23 09:21:21 -0300

    Update light-sensor.ino

--- light-sensor.ino
+++ light-sensor.ino
@@ -1,45 +1,65 @@
-int sensorLeft = 0;
-int sensorRight = 0;
-int valueLeft = 0;
-int valueRight = 0;
-bool stateLeft = false;
-bool stateRight = false;
+#include "I2Cdev.h" 
+#include "MPU6050.h"
+#include "Wire.h"
+
+#define light_port 8
+
+bool light_state = false;
+unsigned int right_counter = 0; 
+
+MPU6050 giroscope_sensor;
 
 void setup() {
-  pinMode(8, INPUT);
-  pinMode(2, INPUT);
   Serial.begin(9600);
+  Wire.begin();
+
+  pinMode(light_port, INPUT);
+
+  giroscope_sensor.initialize();
+  if (giroscope_sensor.testConnection())
+    Serial.println("Sensor initialized");
+  else 
+    Serial.println("Error on sensor initialize");
 }
 
-void counterLeft() {
-    Serial.print("Left: ");
-    Serial.println(valueLeft);
-    valueLeft ++;
-}
+void loop() { 
+  
+  int light_sensor = digitalRead(light_port);
+  
+    if (light_sensor == 1 && !light_state) {
+      light_state = true;
+      Serial.println(right_counter++);
+            
+    } else if (light_sensor == 0 && light_state) {
+      light_state = false;
+    }
+    
+  int ax, ay, az; 
+  int gx, gy, gz;
 
-void counterRight() {
-    Serial.print("Right: ");
-    Serial.println(valueRight);
-    valueRight ++;
-}
+  giroscope_sensor.getAcceleration(&ax, &ay, &az);  
+  giroscope_sensor.getRotation(&gx, &gy, &gz);
 
-void loop() {
-  sensorRight = digitalRead(2);
-  sensorLeft = digitalRead(8);
+  float ax_m_s2 = ax * (9.81/16384.0);
+  float ay_m_s2 = ay * (9.81/16384.0);
+  float az_m_s2 = az * (9.81/16384.0);
+  float gx_deg_s = gx * (250.0/32768.0);
+  float gy_deg_s = gy * (250.0/32768.0);
+  float gz_deg_s = gz * (250.0/32768.0);
 
-  if (sensorRight==1  & !stateRight) {
-    stateRight = true;
-    counterRight();
-  }
-  else if (sensorRight==0  & stateRight) {
-    stateRight = false;
-  }
+  String s = "[";
+  s += String(ax_m_s2);
+  s += " ";
+  s += String(ay_m_s2);
+  s += " ";
+  s += String(az_m_s2);
+  s += "] [";
+  s += String(gx_deg_s);
+  s += " ";
+  s += String(gy_deg_s);
+  s += " ";
+  s += String(gz_deg_s);
+  s += "]";
 
-  if (sensorLeft==1  & !stateLeft) {
-    stateLeft = true;
-    counterLeft();
-  }
-  else if (sensorLeft==0  & stateLeft) {
-    stateLeft = false;
-  }
+  Serial.println(s);
 }
