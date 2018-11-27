028f9c972890d23c754b67f2c6eb1b5743b12804
Michael Costa <macosta@inf.ufpel.edu.br>
2018-11-27 19:56:32 -0200

    fixed port and refactored code
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- light-sensor.ino
+++ light-sensor.ino
@@ -1,27 +1,26 @@
 #include <ros.h>
 #include <std_msgs/String.h>
+#define light_port 8
 
 ros::NodeHandle  node;
 std_msgs::String light_msg;
 ros::Publisher light_pub("right_sensor", &light_msg);
-
 unsigned int right_counter = 0;
 bool light_state = false;
 
 void setup() {
-  pinMode(2, INPUT);
   Serial.begin(9600);
+  pinMode(light_port, INPUT);
   node.initNode();
   node.advertise(light_pub);
 }
 
 void loop() {
-  int light_sensor = digitalRead(2);
+  int light_sensor = digitalRead(light_port);
   if (light_sensor == 1  && !light_state) {
     light_state = true;
-    right_counter++;
     char buffer[5];
-    String(right_counter).toCharArray(buffer, 5);
+    String(right_counter++).toCharArray(buffer, 5);
     light_msg.data = buffer;
     light_pub.publish(&light_msg);
   } else if (light_sensor == 0  && light_state) {
