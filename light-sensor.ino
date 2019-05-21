aa8cb6e161b6a999ed69f15df6dc413e86e2a232
Michael Costa <macosta@inf.ufpel.edu.br>
2019-05-21 14:09:28 -0300

    fixed style
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- light-sensor.ino
+++ light-sensor.ino
@@ -7,17 +7,19 @@
 unsigned int right_counter = 0;
 bool odometry_state = false;
 
-void clearCounter(const std_msgs::Bool &shouldClear) {
+void clearCounter(const std_msgs::Bool &shouldClear)
+{
   if (shouldClear.data)
     right_counter = 0;
 }
 
-ros::NodeHandle  node;
+ros::NodeHandle node;
 std_msgs::Int16 value_odometry;
 ros::Publisher odometry_pub("right_sensor", &value_odometry);
 ros::Subscriber<std_msgs::Bool> clear_counter("pattern", &clearCounter);
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(odometryPin, INPUT);
   node.initNode();
@@ -25,13 +27,17 @@ void setup() {
   node.subscribe(clear_counter);
 }
 
-void loop() {
+void loop()
+{
   int odometry_sensor = digitalRead(odometryPin);
-  if (odometry_sensor == 1  && !odometry_state) {
+  if (odometry_sensor == 1 && !odometry_state)
+  {
     odometry_state = true;
     value_odometry.data = right_counter++;
     odometry_pub.publish(&value_odometry);
-  } else if (odometry_sensor == 0  && odometry_state) {
+  }
+  else if (odometry_sensor == 0 && odometry_state)
+  {
     odometry_state = false;
   }
   node.spinOnce();
