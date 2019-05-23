d308e8b0a0bf1d5c943911a872253481c5106c9c
Michael Costa <macosta@inf.ufpel.edu.br>
2019-05-22 23:11:42 -0300

    fixed variable to suport two sensors
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- light-sensor.ino
+++ light-sensor.ino
@@ -2,10 +2,10 @@
 #include <std_msgs/Int16.h>
 #include <std_msgs/Bool.h>
 
-#define odometryPin 2
+#define odometryPinRight 2
 
 unsigned int right_counter = 0;
-unsigned int tmp_counter = 0;
+unsigned int sensor_counter_right = 0;
 
 void clearCounter(const std_msgs::Bool &shouldClear)
 {
@@ -14,37 +14,37 @@ void clearCounter(const std_msgs::Bool &shouldClear)
 }
 
 ros::NodeHandle node;
-std_msgs::Int16 value_odometry;
-ros::Publisher odometry_pub("right_sensor", &value_odometry);
+std_msgs::Int16 value_odometry_right;
+ros::Publisher odometry_pub_right("right_sensor", &value_odometry_right);
 ros::Subscriber<std_msgs::Bool> clear_counter("pattern", &clearCounter);
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(odometryPin, INPUT);
+  pinMode(odometryPinRight, INPUT);
   node.initNode();
-  node.advertise(odometry_pub);
+  node.advertise(odometry_pub_right);
   node.subscribe(clear_counter);
-  attachInterrupt(digitalPinToInterrupt(odometryPin), counter, RISING);
+  attachInterrupt(digitalPinToInterrupt(odometryPinRight), counterRight, RISING);
 }
 
 void loop()
 {
-  if (right_counter == 10)
+  if (right_counter >= 10)
   {
-    value_odometry.data = right_counter;
-    odometry_pub.publish(&value_odometry);
+    value_odometry_right.data = right_counter;
+    odometry_pub_right.publish(&value_odometry_right);
     right_counter = 0;
   }
   node.spinOnce();
 }
 
-void counter()
+void counterRight()
 {
-  tmp_counter++;
-  if (tmp_counter == 4)
+  sensor_counter_right++;
+  if (sensor_counter_right >= 4)
   {
-    tmp_counter = 0;
+    sensor_counter_right = 0;
     right_counter++;
   }
 }
