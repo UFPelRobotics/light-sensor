5c9d4bfa404ed565b67e1a8808d735ef04507dad
Michael Costa <macosta@inf.ufpel.edu.br>
2019-05-22 23:55:53 -0300

    added left_sensor to project using interrupt
    Signed-off-by: Michael Costa <macosta@inf.ufpel.edu.br>

--- light-sensor.ino
+++ light-sensor.ino
@@ -3,29 +3,40 @@
 #include <std_msgs/Bool.h>
 
 #define odometryPinRight 2
+#define odometryPinLeft 3
 
 unsigned int right_counter = 0;
+unsigned int left_counter = 0;
 unsigned int sensor_counter_right = 0;
+unsigned int sensor_counter_left = 0;
 
 void clearCounter(const std_msgs::Bool &shouldClear)
 {
   if (shouldClear.data)
+  {
     right_counter = 0;
+    left_counter = 0;
+  }
 }
 
 ros::NodeHandle node;
 std_msgs::Int16 value_odometry_right;
+std_msgs::Int16 value_odometry_left;
 ros::Publisher odometry_pub_right("right_sensor", &value_odometry_right);
+ros::Publisher odometry_pub_left("left_sensor", &value_odometry_left);
 ros::Subscriber<std_msgs::Bool> clear_counter("pattern", &clearCounter);
 
 void setup()
 {
   Serial.begin(9600);
   pinMode(odometryPinRight, INPUT);
+  pinMode(odometryPinLeft, INPUT);
   node.initNode();
   node.advertise(odometry_pub_right);
+  node.advertise(odometry_pub_left);
   node.subscribe(clear_counter);
   attachInterrupt(digitalPinToInterrupt(odometryPinRight), counterRight, RISING);
+  attachInterrupt(digitalPinToInterrupt(odometryPinLeft), counterLeft, RISING);
 }
 
 void loop()
@@ -35,6 +46,12 @@ void loop()
     value_odometry_right.data = right_counter;
     odometry_pub_right.publish(&value_odometry_right);
     right_counter = 0;
+  }
+  if (left_counter >= 10)
+  {
+    value_odometry_left.data = left_counter;
+    odometry_pub_left.publish(&value_odometry_left);
+    left_counter = 0;
   }
   node.spinOnce();
 }
@@ -46,5 +63,15 @@ void counterRight()
   {
     sensor_counter_right = 0;
     right_counter++;
+  }
+}
+
+void counterLeft()
+{
+  sensor_counter_left++;
+  if (sensor_counter_left >= 4)
+  {
+    sensor_counter_left = 0;
+    left_counter++;
   }
 }
